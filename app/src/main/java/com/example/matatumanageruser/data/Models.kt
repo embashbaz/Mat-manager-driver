41908d893678c7ce1b4bb2612273cfe1a16ba1de
Emery Bashige <bemeryb@gmail.com>
2021-09-23 23:53:32 +0300

    added model classes

---
+++ app/src/main/java/com/example/matatumanageruser/data/Models.kt
@@ -0,0 +1,59 @@
+package com.example.matatumanageruser.data
+
+
+
+import com.google.firebase.firestore.GeoPoint
+import java.util.*
+
+
+data class  MatatuAdmin(
+    var saccoId: String = "",
+    var saccoEmail : String = "",
+    var compName: String = "",
+    var countyName: String = ""
+)
+
+data class  Matatu(
+    var matId: String = "",
+    var matPlate: String = "",
+    var matNumber: String = "",
+    var brand: String = ""
+
+)
+
+data class Driver(
+    var driverId: String = "",
+    var driverName: String = "",
+    var drivePhone: Long = 0L
+)
+
+data class DayStatistic(
+    var dayId: String = "",
+    var matId: String = "",
+    var day: Date? = null,
+    var startTime: String = "",
+    var endTime: String = "",
+    var location: GeoPoint? = null,
+    var maxSpeed: Double = 0.0,
+    var distanceCovered: Double = 0.0,
+    //var placeCovered: .....
+    var amountCollected: Double = 0.0,
+    var expenduture: MutableList<MatatuExpenduture> = mutableListOf()
+)
+
+data class MatatuExpenduture(
+    var expendutureName : String = "",
+    var expendutureAmount: Double = 0.0,
+    var expendutureDescription: String = ""
+)
+
+data class MatatuTrip(
+    var tripId: String = "",
+    var tripDay: Date? = null,
+    var carId: String = "",
+    var driverId: String = "",
+    var startTime: String = "",
+    var endTime: String = "",
+    var location: GeoPoint? = null,
+    var amountCollected: Double = 0.0
+)
