a36e17a4b7dfbe2a9107ec67fed3ab7cd6c7550f
Emery Bashige <bemeryb@gmail.com>
2021-10-29 11:02:55 +0300

    added routes path

---
+++ app/src/main/java/com/example/matatumanageruser/utils/RoutesName.kt
@@ -0,0 +1,18 @@
+package com.example.util
+
+
+const val API_VERSION = "/v1"
+const val MAT_ADMIN = "$API_VERSION/mat_admin"
+const val DRIVERS = "$API_VERSION/driver"
+const val BUSES = "$API_VERSION/bus"
+const val TRIPS = "$API_VERSION/trip"
+const val STATS = "$API_VERSION/stat"
+const val EXPENSES = "$API_VERSION/expense"
+const val CREATE_TRIPS = "$TRIPS/create"
+const val CREATE_STATS = "$STATS/create"
+const val CREATE_EXPENSES = "$EXPENSES/create"
+const val UPDATE_BUSES = "$BUSES/update"
+const val UPDATE_TRIPS = "$TRIPS/update"
+const val UPDATE_STATS = "$STATS/update"
+
+
