6fce29b93d40b392b2cc97d48c0b02033c332250
Emery Bashige <bemeryb@gmail.com>
2021-11-18 22:29:43 +0300

    added some dashboard method

--- app/src/main/java/com/example/matatumanageruser/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/matatumanageruser/ui/dashboard/DashboardFragment.kt
@@ -5,17 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.matatumanageruser.R
+import com.example.matatumanageruser.databinding.FragmentDashboardBinding
 
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardBinding: FragmentDashboardBinding
+    private val dashboardViewModel: DashboardViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        dashboardBinding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val view = dashboardBinding.root
+
+        observeCardClicked()
+        listenButtonClicked()
+
+
+        return view
+
+    }
+
+    private fun listenButtonClicked() {
+       dashboardBinding.goToIssuesCard.setOnClickListener {
+           dashboardViewModel.issueCardClicked(true)
+       }
+
+        dashboardBinding.goToExpensesList.setOnClickListener {
+            dashboardViewModel.expenseCardClicked(true)
+        }
+
+        dashboardBinding.goTripCard.setOnClickListener {  }
+
+        dashboardBinding.goToPerformanceCard.setOnClickListener {  }
+
+        dashboardBinding.startDayCard.setOnClickListener {  }
+
+    }
+
+    private fun observeCardClicked() {
+
+
+        dashboardViewModel.issuesCardClicked.observe(viewLifecycleOwner, {
+            if (it){
+                this.findNavController().navigate(R.id.action_dashboardFragment_to_issueFragment)
+                dashboardViewModel.issueCardClicked(false)
+            }
+        })
+
+        dashboardViewModel.expenseCardClicked.observe(viewLifecycleOwner, {
+            if (it){
+                this.findNavController().navigate(R.id.action_dashboardFragment_to_expenseListFragment)
+                dashboardViewModel.expenseCardClicked(false)
+            }
+        })
     }
 
 
---
+++ app/src/main/java/com/example/matatumanageruser/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.matatumanageruser.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.matatumanageradmin.data.MainRepository
+import com.example.matatumanageradmin.utils.DispatcherProvider
+import com.example.matatumanageruser.data.MainRepository
+import com.example.matatumanageruser.utils.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DashboardViewModel  @Inject
+constructor(private var repository: MainRepository,
+            private val dispatcher: DispatcherProvider
+): ViewModel() {
+
+    private var _expenseCardClicked = MutableLiveData(false)
+    val expenseCardClicked: LiveData<Boolean>
+            get() = _expenseCardClicked
+
+    private var _profileCardClicked = MutableLiveData(false)
+    val profileCardClicked: LiveData<Boolean>
+        get() = _profileCardClicked
+
+    private var _issuesCardClicked = MutableLiveData(false)
+    val issuesCardClicked: LiveData<Boolean>
+        get() = _issuesCardClicked
+
+    private var _tripCardClicked = MutableLiveData(false)
+    val tripCardClicked: LiveData<Boolean>
+        get() = _tripCardClicked
+
+    fun expenseCardClicked(action: Boolean){
+        _expenseCardClicked.value = action
+    }
+
+    fun issueCardClicked(action: Boolean){
+        _issuesCardClicked.value = action
+    }
+
+    fun tripCardClicked(action: Boolean){
+        _tripCardClicked.value = action
+    }
+
+    fun profileCardClicked(action: Boolean){
+        _profileCardClicked.value = action
+    }
+
+
+}
