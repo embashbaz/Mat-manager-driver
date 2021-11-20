6b401406c6b656233be15fd8932be611d119072e
Emery Bashige <bemeryb@gmail.com>
2021-11-20 10:49:50 +0300

    added map view to tripFragment and added lifecycle methods for the fragment

--- app/src/main/java/com/example/matatumanageruser/ui/trip/TripFragment.kt
+++ app/src/main/java/com/example/matatumanageruser/ui/trip/TripFragment.kt
@@ -6,17 +6,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.matatumanageruser.R
+import com.example.matatumanageruser.databinding.FragmentTripBinding
+import com.example.matatumanageruser.databinding.FragmentTripListBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 
 class TripFragment : Fragment() {
 
+    lateinit var tripBinding: FragmentTripBinding
+    lateinit var mapView: MapView
+
+    private var map: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip, container, false)
+       tripBinding = FragmentTripBinding.inflate(inflater,container, false )
+        mapView = tripBinding.projectMapView
+        val view = tripBinding.root
+
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 
 }
