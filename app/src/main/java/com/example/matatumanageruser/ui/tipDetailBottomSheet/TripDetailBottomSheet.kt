b694c0b966902d3f43122318d89d334fb72d98de
Emery Bashige <bemeryb@gmail.com>
2021-11-22 13:55:18 +0300

    added trip viewModel and updated trip detail

--- app/src/main/java/com/example/matatumanageruser/ui/tipDetailBottomSheet/TripDetailBottomSheet.kt
+++ app/src/main/java/com/example/matatumanageruser/ui/tipDetailBottomSheet/TripDetailBottomSheet.kt
@@ -2,9 +2,13 @@ package com.example.matatumanageruser.ui.tipDetailBottomSheet
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
 import com.example.matatumanageruser.data.Trip
 import com.example.matatumanageruser.databinding.TripDetailsBinding
+import com.example.matatumanageruser.ui.other.showLongToast
+import com.example.matatumanageruser.ui.other.stringFromTl
+import com.example.matatumanageruser.ui.trip.TripViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class TripDetailBottomSheet(var trip: Trip): BottomSheetDialogFragment() {
@@ -44,8 +48,26 @@ class TripDetailBottomSheet(var trip: Trip): BottomSheetDialogFragment() {
     private fun endTrip() {
         tripDetailDialogBinding.startTripBt.setText("End trip")
         tripDetailDialogBinding.startTripBt.setOnClickListener {
+            if(trip.moneyCollected < 1.0){
+                if (stringFromTl(tripDetailDialogBinding.amountTl).isNotEmpty()){
+                    val actualAmount = stringFromTl(tripDetailDialogBinding.amountTl).toDoubleOrNull()
+                    if (actualAmount == null){
+                        tripDetailDialogBinding.amountTl.boxStrokeColor = Color.RED
+                        showLongToast("invalid number")
+                        return@setOnClickListener
+                    }else{
+                        trip.moneyCollected = actualAmount
+                    }
+                }else{
+                    tripDetailDialogBinding.amountTl.boxStrokeColor = Color.RED
+                    showLongToast("Amount of money collected must not be 0")
+                    return@setOnClickListener
+                }
+            }
+
             trip.tripStatus = "ended"
             listener.onStartTripClicked(trip, false)
+            dialog?.dismiss()
 
         }
     }
---
+++ app/src/main/java/com/example/matatumanageruser/ui/trip/TripViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.matatumanageruser.ui.trip
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.matatumanageruser.data.Expense
+import com.example.matatumanageruser.data.MainRepository
+import com.example.matatumanageruser.data.Trip
+import com.example.matatumanageruser.ui.other.getDate
+import com.example.matatumanageruser.utils.DispatcherProvider
+import com.example.matatumanageruser.utils.OperationStatus
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class TripViewModel  @Inject constructor(val repository: MainRepository,
+                                         private val dispatcher: DispatcherProvider
+) : ViewModel(){
+
+    private var _createNewTrip = MutableLiveData<TripStatus>(TripStatus.Empty)
+    val createNewTrip : LiveData<TripStatus>
+            get() = _createNewTrip
+
+    private var _updateTrip = MutableLiveData<TripStatus>(TripStatus.Empty)
+    val updateTrip: LiveData<TripStatus>
+        get() = _updateTrip
+
+
+    fun createTrip(pickUpPoint: String, amount: String, busPlate: String, driverId: String){
+
+        val actualAmount = amount.toDoubleOrNull()
+        if (actualAmount == null && amount.isNotEmpty()){
+            _createNewTrip.postValue(TripStatus.Failed("invalid value passed for fare collected"))
+            return
+        }
+
+        viewModelScope.launch(dispatcher.io){
+            _createNewTrip.postValue(TripStatus.Loading)
+            val trip = Trip(getDate(), "", busPlate, driverId, "", pickUpPoint, actualAmount?: 0.0,
+                       getDate(), "", "started", "")
+            when(val response = repository.addTrip(trip)){
+                is OperationStatus.Error -> _createNewTrip.postValue(TripStatus.Failed(response.message!!))
+                is OperationStatus.Success -> _createNewTrip.postValue(TripStatus.Success("Trip Started", trip))
+
+            }
+        }
+    }
+
+    fun updateTrip(trip: Trip){
+        viewModelScope.launch(dispatcher.io){
+            _updateTrip.postValue(TripStatus.Loading)
+            when(val response = repository.updateTrip(trip)){
+                is OperationStatus.Error -> _updateTrip.postValue(TripStatus.Failed(response.message!!))
+                is OperationStatus.Success -> _updateTrip.postValue(TripStatus.Success("Trip updated", null))
+
+            }
+        }
+    }
+
+
+
+
+    sealed class TripStatus{
+        class Success(val resultText: String, val trip: Trip?): TripStatus()
+        class Failed(val errorText: String): TripStatus()
+        object Loading: TripStatus()
+        object Empty: TripStatus()
+    }
+
+
+}
